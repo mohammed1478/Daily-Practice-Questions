0ae49980ed0eade7a2dad4e8a93babf2f9acd128
Mohammed Chowdhury <41757010+mohammed1478@users.noreply.github.com>
2019-04-10 17:36:12 -0400

    Question #2

---
+++ Question #2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+/* Given an array of integers, return a new array
+such that each element at index i of the new array is the product of all
+the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+[120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].*/
+
+void  multOfArray (int a[], int n);
+
+int main(){
+  int n;
+  cout << "size of array?\n";
+  cin >> n;
+  int *a= new int[n];
+  cout << "enter elements : (PRESS ENTER AFTER EACH ELEMENT) " << endl;
+
+  for (int i = 0; i < n; i++){
+    cin >> a[i];
+  }
+  multOfArray(a,n);
+  delete []a;
+}
+
+void  multOfArray (int a[], int n){
+  for (int i = 0; i < n; i++){
+    int ans =1;
+    for (int j = 0; j<n; j++){
+      if (j != i){
+        ans *= a[j];
+      }
+    }
+    cout << ans<< " ";
+  }
+}
